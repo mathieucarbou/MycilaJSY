cb9eeb36090200f5a3bb319df72779e22a281208
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-06-23 01:18:57 +0200

    v9.0.7

--- src/MycilaJSY.h
+++ src/MycilaJSY.h
@@ -12,10 +12,10 @@
   #include <ArduinoJson.h>
 #endif
 
-#define MYCILA_JSY_VERSION          "9.0.6"
+#define MYCILA_JSY_VERSION          "9.0.7"
 #define MYCILA_JSY_VERSION_MAJOR    9
 #define MYCILA_JSY_VERSION_MINOR    0
-#define MYCILA_JSY_VERSION_REVISION 6
+#define MYCILA_JSY_VERSION_REVISION 7
 
 #ifndef MYCILA_JSY_ASYNC_CORE
   #define MYCILA_JSY_ASYNC_CORE 0
