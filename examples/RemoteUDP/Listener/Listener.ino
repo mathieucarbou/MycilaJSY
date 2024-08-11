2ed82dd7499da045d4bb7b6814b255427240d932
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-08-11 18:45:39 +0200

    mathieucarbou/MycilaESPConnect @ 5.0.0

--- examples/RemoteUDP/Listener/Listener.ino
+++ examples/RemoteUDP/Listener/Listener.ino
@@ -72,7 +72,7 @@
 #include <ESPDash.h>              // https://github.com/mathieucarbou/ayushsharma82-ESP-DASH#dev
 #include <FastCRC32.h>            // https://github.com/RobTillaart/CRC
 #include <MycilaCircularBuffer.h> // https://github.com/mathieucarbou/MycilaUtilities
-#include <MycilaESPConnect.h>     // https://github.com/mathieucarbou/MycilaESPConnect
+#include <MycilaespConnect.h>     // https://github.com/mathieucarbou/MycilaESPConnect
 #include <MycilaJSY.h>            // https://github.com/mathieucarbou/MycilaJSY
 #include <MycilaLogger.h>         // https://github.com/mathieucarbou/MycilaLogger
 #include <MycilaSystem.h>         // https://github.com/mathieucarbou/MycilaSystem
@@ -82,6 +82,7 @@
 
 AsyncUDP udp;
 AsyncWebServer webServer(80);
+Mycila::ESPConnect espConnect(webServer);
 ESPDash dashboard = ESPDash(&webServer, "/dashboard", false);
 Mycila::Logger logger;
 Mycila::TaskManager coreTaskManager("core");
@@ -140,7 +141,7 @@ volatile Mycila::JSYData jsyData;
 Mycila::CircularBuffer<float, MYCILA_UDP_SEND_RATE_WINDOW> messageRateBuffer;
 volatile float messageRate = 0;
 
-Mycila::Task networkManagerTask("ESPConnect", [](void* params) { ESPConnect.loop(); });
+Mycila::Task networkManagerTask("ESPConnect", [](void* params) { espConnect.loop(); });
 
 Mycila::Task networkUpTask("Network UP", Mycila::TaskType::ONCE, [](void* params) {
   logger.info(TAG, "Enable Network Services...");
@@ -173,15 +174,15 @@ Mycila::Task restartTask("Restart", Mycila::TaskType::ONCE, [](void* params) {
 });
 
 Mycila::Task dashboardTask("Dashboard", [](void* params) {
-  ESPConnectMode mode = ESPConnect.getMode();
+  Mycila::ESPConnect::Mode mode = espConnect.getMode();
 
-  networkAPIP.set(ESPConnect.getIPAddress(ESPConnectMode::AP).toString().c_str());
-  networkEthIP.set(ESPConnect.getIPAddress(ESPConnectMode::ETH).toString().c_str());
-  networkInterface.set(mode == ESPConnectMode::AP ? "AP" : (mode == ESPConnectMode::STA ? "WiFi" : (mode == ESPConnectMode::ETH ? "Ethernet" : "")));
-  networkWiFiIP.set(ESPConnect.getIPAddress(ESPConnectMode::STA).toString().c_str());
-  networkWiFiRSSI.set((String(ESPConnect.getWiFiRSSI()) + " dBm").c_str());
-  networkWiFiSignal.set((String(ESPConnect.getWiFiSignalQuality()) + " %").c_str());
-  networkWiFiSSID.set(ESPConnect.getWiFiSSID().c_str());
+  networkAPIP.set(espConnect.getIPAddress(Mycila::ESPConnect::Mode::AP).toString().c_str());
+  networkEthIP.set(espConnect.getIPAddress(Mycila::ESPConnect::Mode::ETH).toString().c_str());
+  networkInterface.set(mode == Mycila::ESPConnect::Mode::AP ? "AP" : (mode == Mycila::ESPConnect::Mode::STA ? "WiFi" : (mode == Mycila::ESPConnect::Mode::ETH ? "Ethernet" : "")));
+  networkWiFiIP.set(espConnect.getIPAddress(Mycila::ESPConnect::Mode::STA).toString().c_str());
+  networkWiFiRSSI.set((String(espConnect.getWiFiRSSI()) + " dBm").c_str());
+  networkWiFiSignal.set((String(espConnect.getWiFiSignalQuality()) + " %").c_str());
+  networkWiFiSSID.set(espConnect.getWiFiSSID().c_str());
   uptime.set(Mycila::Time::toDHHMMSS(Mycila::System.getUptime()).c_str());
 
   activePower1.update(jsyData.power1);
@@ -251,7 +252,7 @@ void setup() {
   Mycila::System.begin();
 
   // tasks
-  dashboardTask.setEnabledWhen([]() { return ESPConnect.isConnected() && !dashboard.isAsyncAccessInProgress(); });
+  dashboardTask.setEnabledWhen([]() { return espConnect.isConnected() && !dashboard.isAsyncAccessInProgress(); });
   dashboardTask.setInterval(1000 * Mycila::TaskDuration::MILLISECONDS);
   dashboardTask.setManager(coreTaskManager);
   Mycila::TaskManager::configureWDT();
@@ -279,12 +280,12 @@ void setup() {
   ElegantOTA.begin(&webServer, MYCILA_ADMIN_USERNAME, MYCILA_ADMIN_PASSWORD);
 
   // Dashboard
-  webServer.rewrite("/", "/dashboard").setFilter([](AsyncWebServerRequest* request) { return ESPConnect.getState() != ESPConnectState::PORTAL_STARTED; });
+  webServer.rewrite("/", "/dashboard").setFilter([](AsyncWebServerRequest* request) { return espConnect.getState() != Mycila::ESPConnect::State::PORTAL_STARTED; });
 
   restart.attachCallback([](int32_t value) { restartTask.resume(); });
 
   reset.attachCallback([](int32_t value) {
-    ESPConnect.clearConfiguration();
+    espConnect.clearConfiguration();
     restartTask.resume();
   });
 
@@ -296,10 +297,10 @@ void setup() {
   dashboard.onBeforeUpdate([](bool changes_only) {
     if (!changes_only) {
       logger.debug(TAG, "Dashboard refresh requested");
-      networkAPMAC.set(ESPConnect.getMACAddress(ESPConnectMode::AP).c_str());
-      networkEthMAC.set(ESPConnect.getMACAddress(ESPConnectMode::ETH).isEmpty() ? "N/A" : ESPConnect.getMACAddress(ESPConnectMode::ETH).c_str());
+      networkAPMAC.set(espConnect.getMACAddress(Mycila::ESPConnect::Mode::AP).c_str());
+      networkEthMAC.set(espConnect.getMACAddress(Mycila::ESPConnect::Mode::ETH).isEmpty() ? "N/A" : espConnect.getMACAddress(Mycila::ESPConnect::Mode::ETH).c_str());
       networkHostname.set(hostname.c_str());
-      networkWiFiMAC.set(ESPConnect.getMACAddress(ESPConnectMode::STA).c_str());
+      networkWiFiMAC.set(espConnect.getMACAddress(Mycila::ESPConnect::Mode::STA).c_str());
     }
   });
   dashboard.setAuthentication(MYCILA_ADMIN_USERNAME, MYCILA_ADMIN_PASSWORD);
@@ -356,59 +357,59 @@ void setup() {
   });
 
   // Network Manager
-  ESPConnect.setAutoRestart(true);
-  ESPConnect.setBlocking(false);
-  ESPConnect.listen([](ESPConnectState previous, ESPConnectState state) {
-    logger.debug(TAG, "NetworkState: %s => %s", ESPConnect.getStateName(previous), ESPConnect.getStateName(state));
+  espConnect.setAutoRestart(true);
+  espConnect.setBlocking(false);
+  espConnect.listen([](Mycila::ESPConnect::State previous, Mycila::ESPConnect::State state) {
+    logger.debug(TAG, "NetworkState: %s => %s", espConnect.getStateName(previous), espConnect.getStateName(state));
     switch (state) {
-      case ESPConnectState::NETWORK_DISABLED:
+      case Mycila::ESPConnect::State::NETWORK_DISABLED:
         logger.warn(TAG, "Disabled Network!");
         break;
-      case ESPConnectState::AP_STARTING:
-        logger.info(TAG, "Starting Access Point %s...", ESPConnect.getAccessPointSSID().c_str());
+      case Mycila::ESPConnect::State::AP_STARTING:
+        logger.info(TAG, "Starting Access Point %s...", espConnect.getAccessPointSSID().c_str());
         break;
-      case ESPConnectState::AP_STARTED:
-        logger.info(TAG, "Access Point %s started with IP address %s", ESPConnect.getWiFiSSID().c_str(), ESPConnect.getIPAddress().toString().c_str());
+      case Mycila::ESPConnect::State::AP_STARTED:
+        logger.info(TAG, "Access Point %s started with IP address %s", espConnect.getWiFiSSID().c_str(), espConnect.getIPAddress().toString().c_str());
         networkUpTask.resume();
         break;
-      case ESPConnectState::NETWORK_CONNECTING:
+      case Mycila::ESPConnect::State::NETWORK_CONNECTING:
         logger.info(TAG, "Connecting to network...");
         break;
-      case ESPConnectState::NETWORK_CONNECTED:
-        logger.info(TAG, "Connected with IP address %s", ESPConnect.getIPAddress().toString().c_str());
+      case Mycila::ESPConnect::State::NETWORK_CONNECTED:
+        logger.info(TAG, "Connected with IP address %s", espConnect.getIPAddress().toString().c_str());
         networkUpTask.resume();
         break;
-      case ESPConnectState::NETWORK_TIMEOUT:
+      case Mycila::ESPConnect::State::NETWORK_TIMEOUT:
         logger.warn(TAG, "Unable to connect!");
         break;
-      case ESPConnectState::NETWORK_DISCONNECTED:
+      case Mycila::ESPConnect::State::NETWORK_DISCONNECTED:
         logger.warn(TAG, "Disconnected!");
         break;
-      case ESPConnectState::NETWORK_RECONNECTING:
+      case Mycila::ESPConnect::State::NETWORK_RECONNECTING:
         logger.info(TAG, "Trying to reconnect...");
         break;
-      case ESPConnectState::PORTAL_STARTING:
-        logger.info(TAG, "Starting Captive Portal %s for %" PRIu32 " seconds...", ESPConnect.getAccessPointSSID().c_str(), ESPConnect.getCaptivePortalTimeout());
+      case Mycila::ESPConnect::State::PORTAL_STARTING:
+        logger.info(TAG, "Starting Captive Portal %s for %" PRIu32 " seconds...", espConnect.getAccessPointSSID().c_str(), espConnect.getCaptivePortalTimeout());
         break;
-      case ESPConnectState::PORTAL_STARTED:
-        logger.info(TAG, "Captive Portal started at %s with IP address %s", ESPConnect.getWiFiSSID().c_str(), ESPConnect.getIPAddress().toString().c_str());
+      case Mycila::ESPConnect::State::PORTAL_STARTED:
+        logger.info(TAG, "Captive Portal started at %s with IP address %s", espConnect.getWiFiSSID().c_str(), espConnect.getIPAddress().toString().c_str());
         break;
-      case ESPConnectState::PORTAL_COMPLETE: {
-        if (ESPConnect.hasConfiguredAPMode()) {
+      case Mycila::ESPConnect::State::PORTAL_COMPLETE: {
+        if (espConnect.hasConfiguredAPMode()) {
           logger.info(TAG, "Captive Portal: Access Point configured");
         } else {
           logger.info(TAG, "Captive Portal: WiFi configured");
         }
         break;
       }
-      case ESPConnectState::PORTAL_TIMEOUT:
+      case Mycila::ESPConnect::State::PORTAL_TIMEOUT:
         logger.warn(TAG, "Captive Portal: timed out.");
         break;
       default:
         break;
     }
   });
-  ESPConnect.begin(webServer, hostname, ssid, MYCILA_ADMIN_PASSWORD);
+  espConnect.begin(hostname, ssid, MYCILA_ADMIN_PASSWORD);
 
   // start tasks
   assert(coreTaskManager.asyncStart(1024 * 4, 1, 1, 100, true)); // NOLINT
--- examples/RemoteUDP/Sender/Sender.ino
+++ examples/RemoteUDP/Sender/Sender.ino
@@ -85,7 +85,7 @@
 #include <ElegantOTA.h>           // https://github.com/ayushsharma82/ElegantOTA
 #include <FastCRC32.h>            // https://github.com/RobTillaart/CRC
 #include <MycilaCircularBuffer.h> // https://github.com/mathieucarbou/MycilaUtilities
-#include <MycilaESPConnect.h>     // https://github.com/mathieucarbou/MycilaESPConnect
+#include <MycilaespConnect.h>     // https://github.com/mathieucarbou/MycilaESPConnect
 #include <MycilaJSY.h>            // https://github.com/mathieucarbou/MycilaJSY
 #include <MycilaLogger.h>         // https://github.com/mathieucarbou/MycilaLogger
 #include <MycilaSystem.h>         // https://github.com/mathieucarbou/MycilaSystem
@@ -95,6 +95,7 @@
 
 AsyncUDP udp;
 AsyncWebServer webServer(80);
+Mycila::ESPConnect espConnect(webServer);
 ESPDash dashboard = ESPDash(&webServer, "/dashboard", false);
 Mycila::JSY jsy;
 Mycila::Logger logger;
@@ -165,7 +166,7 @@ Mycila::CircularBuffer<uint32_t, MYCILA_UDP_SEND_RATE_WINDOW> dataRateBuffer;
 volatile uint32_t dataRate = 0;
 
 Mycila::Task jsyTask("JSY", [](void* params) { jsy.read(); });
-Mycila::Task networkManagerTask("ESPConnect", [](void* params) { ESPConnect.loop(); });
+Mycila::Task networkManagerTask("ESPConnect", [](void* params) { espConnect.loop(); });
 
 Mycila::Task profilerTask("Profiler", [](void* params) {
   jsyTaskManager.log();
@@ -199,15 +200,15 @@ Mycila::Task restartTask("Restart", Mycila::TaskType::ONCE, [](void* params) {
 });
 
 Mycila::Task dashboardTask("Dashboard", [](void* params) {
-  ESPConnectMode mode = ESPConnect.getMode();
+  Mycila::ESPConnect::Mode mode = espConnect.getMode();
 
-  networkAPIP.set(ESPConnect.getIPAddress(ESPConnectMode::AP).toString().c_str());
-  networkEthIP.set(ESPConnect.getIPAddress(ESPConnectMode::ETH).toString().c_str());
-  networkInterface.set(mode == ESPConnectMode::AP ? "AP" : (mode == ESPConnectMode::STA ? "WiFi" : (mode == ESPConnectMode::ETH ? "Ethernet" : "")));
-  networkWiFiIP.set(ESPConnect.getIPAddress(ESPConnectMode::STA).toString().c_str());
-  networkWiFiRSSI.set((String(ESPConnect.getWiFiRSSI()) + " dBm").c_str());
-  networkWiFiSignal.set((String(ESPConnect.getWiFiSignalQuality()) + " %").c_str());
-  networkWiFiSSID.set(ESPConnect.getWiFiSSID().c_str());
+  networkAPIP.set(espConnect.getIPAddress(Mycila::ESPConnect::Mode::AP).toString().c_str());
+  networkEthIP.set(espConnect.getIPAddress(Mycila::ESPConnect::Mode::ETH).toString().c_str());
+  networkInterface.set(mode == Mycila::ESPConnect::Mode::AP ? "AP" : (mode == Mycila::ESPConnect::Mode::STA ? "WiFi" : (mode == Mycila::ESPConnect::Mode::ETH ? "Ethernet" : "")));
+  networkWiFiIP.set(espConnect.getIPAddress(Mycila::ESPConnect::Mode::STA).toString().c_str());
+  networkWiFiRSSI.set((String(espConnect.getWiFiRSSI()) + " dBm").c_str());
+  networkWiFiSignal.set((String(espConnect.getWiFiSignalQuality()) + " %").c_str());
+  networkWiFiSSID.set(espConnect.getWiFiSSID().c_str());
   uptime.set(Mycila::Time::toDHHMMSS(Mycila::System.getUptime()).c_str());
 
   activePower1.update(jsy.getPower1());
@@ -280,7 +281,7 @@ void setup() {
   Mycila::System.begin();
 
   // tasks
-  dashboardTask.setEnabledWhen([]() { return ESPConnect.isConnected() && !dashboard.isAsyncAccessInProgress(); });
+  dashboardTask.setEnabledWhen([]() { return espConnect.isConnected() && !dashboard.isAsyncAccessInProgress(); });
   dashboardTask.setInterval(1000 * Mycila::TaskDuration::MILLISECONDS);
   dashboardTask.setManager(coreTaskManager);
   jsyTask.setEnabledWhen([]() { return jsy.isEnabled(); });
@@ -315,14 +316,14 @@ void setup() {
   ElegantOTA.begin(&webServer, MYCILA_ADMIN_USERNAME, MYCILA_ADMIN_PASSWORD);
 
   // Dashboard
-  webServer.rewrite("/", "/dashboard").setFilter([](AsyncWebServerRequest* request) { return ESPConnect.getState() != ESPConnectState::PORTAL_STARTED; });
+  webServer.rewrite("/", "/dashboard").setFilter([](AsyncWebServerRequest* request) { return espConnect.getState() != Mycila::ESPConnect::State::PORTAL_STARTED; });
 
   version.set(MYCILA_JSY_VERSION);
 
   restart.attachCallback([](int32_t value) { restartTask.resume(); });
 
   reset.attachCallback([](int32_t value) {
-    ESPConnect.clearConfiguration();
+    espConnect.clearConfiguration();
     restartTask.resume();
   });
 
@@ -346,10 +347,10 @@ void setup() {
   dashboard.onBeforeUpdate([](bool changes_only) {
     if (!changes_only) {
       logger.debug(TAG, "Dashboard refresh requested");
-      networkAPMAC.set(ESPConnect.getMACAddress(ESPConnectMode::AP).c_str());
-      networkEthMAC.set(ESPConnect.getMACAddress(ESPConnectMode::ETH).isEmpty() ? "N/A" : ESPConnect.getMACAddress(ESPConnectMode::ETH).c_str());
+      networkAPMAC.set(espConnect.getMACAddress(Mycila::ESPConnect::Mode::AP).c_str());
+      networkEthMAC.set(espConnect.getMACAddress(Mycila::ESPConnect::Mode::ETH).isEmpty() ? "N/A" : espConnect.getMACAddress(Mycila::ESPConnect::Mode::ETH).c_str());
       networkHostname.set(hostname.c_str());
-      networkWiFiMAC.set(ESPConnect.getMACAddress(ESPConnectMode::STA).c_str());
+      networkWiFiMAC.set(espConnect.getMACAddress(Mycila::ESPConnect::Mode::STA).c_str());
     }
   });
   dashboard.setAuthentication(MYCILA_ADMIN_USERNAME, MYCILA_ADMIN_PASSWORD);
@@ -366,9 +367,9 @@ void setup() {
 
     if (eventType == Mycila::JSYEventType::EVT_CHANGE) {
 
-      ESPConnectMode mode = ESPConnect.getMode();
+      Mycila::ESPConnect::Mode mode = espConnect.getMode();
 
-      if (mode != ESPConnectMode::NONE) {
+      if (mode != Mycila::ESPConnect::Mode::NONE) {
         JsonDocument doc;
         JsonObject root = doc.to<JsonObject>();
         root["c1"] = jsy.getCurrent1();
@@ -404,13 +405,13 @@ void setup() {
 
         // send
         switch (mode) {
-          case ESPConnectMode::AP:
+          case Mycila::ESPConnect::Mode::AP:
             udp.broadcastTo(buffer, packetSize, MYCILA_UDP_PORT, tcpip_adapter_if_t::TCPIP_ADAPTER_IF_AP);
             break;
-          case ESPConnectMode::STA:
+          case Mycila::ESPConnect::Mode::STA:
             udp.broadcastTo(buffer, packetSize, MYCILA_UDP_PORT, tcpip_adapter_if_t::TCPIP_ADAPTER_IF_STA);
             break;
-          case ESPConnectMode::ETH:
+          case Mycila::ESPConnect::Mode::ETH:
             udp.broadcastTo(buffer, packetSize, MYCILA_UDP_PORT, tcpip_adapter_if_t::TCPIP_ADAPTER_IF_ETH);
             break;
           default:
@@ -435,59 +436,59 @@ void setup() {
     jsy.setBaudRate(Mycila::JSYBaudRate::BAUD_38400);
 
   // Network Manager
-  ESPConnect.setAutoRestart(true);
-  ESPConnect.setBlocking(false);
-  ESPConnect.listen([](ESPConnectState previous, ESPConnectState state) {
-    logger.debug(TAG, "NetworkState: %s => %s", ESPConnect.getStateName(previous), ESPConnect.getStateName(state));
+  espConnect.setAutoRestart(true);
+  espConnect.setBlocking(false);
+  espConnect.listen([](Mycila::ESPConnect::State previous, Mycila::ESPConnect::State state) {
+    logger.debug(TAG, "NetworkState: %s => %s", espConnect.getStateName(previous), espConnect.getStateName(state));
     switch (state) {
-      case ESPConnectState::NETWORK_DISABLED:
+      case Mycila::ESPConnect::State::NETWORK_DISABLED:
         logger.warn(TAG, "Disabled Network!");
         break;
-      case ESPConnectState::AP_STARTING:
-        logger.info(TAG, "Starting Access Point %s...", ESPConnect.getAccessPointSSID().c_str());
+      case Mycila::ESPConnect::State::AP_STARTING:
+        logger.info(TAG, "Starting Access Point %s...", espConnect.getAccessPointSSID().c_str());
         break;
-      case ESPConnectState::AP_STARTED:
-        logger.info(TAG, "Access Point %s started with IP address %s", ESPConnect.getWiFiSSID().c_str(), ESPConnect.getIPAddress().toString().c_str());
+      case Mycila::ESPConnect::State::AP_STARTED:
+        logger.info(TAG, "Access Point %s started with IP address %s", espConnect.getWiFiSSID().c_str(), espConnect.getIPAddress().toString().c_str());
         networkUpTask.resume();
         break;
-      case ESPConnectState::NETWORK_CONNECTING:
+      case Mycila::ESPConnect::State::NETWORK_CONNECTING:
         logger.info(TAG, "Connecting to network...");
         break;
-      case ESPConnectState::NETWORK_CONNECTED:
-        logger.info(TAG, "Connected with IP address %s", ESPConnect.getIPAddress().toString().c_str());
+      case Mycila::ESPConnect::State::NETWORK_CONNECTED:
+        logger.info(TAG, "Connected with IP address %s", espConnect.getIPAddress().toString().c_str());
         networkUpTask.resume();
         break;
-      case ESPConnectState::NETWORK_TIMEOUT:
+      case Mycila::ESPConnect::State::NETWORK_TIMEOUT:
         logger.warn(TAG, "Unable to connect!");
         break;
-      case ESPConnectState::NETWORK_DISCONNECTED:
+      case Mycila::ESPConnect::State::NETWORK_DISCONNECTED:
         logger.warn(TAG, "Disconnected!");
         break;
-      case ESPConnectState::NETWORK_RECONNECTING:
+      case Mycila::ESPConnect::State::NETWORK_RECONNECTING:
         logger.info(TAG, "Trying to reconnect...");
         break;
-      case ESPConnectState::PORTAL_STARTING:
-        logger.info(TAG, "Starting Captive Portal %s for %" PRIu32 " seconds...", ESPConnect.getAccessPointSSID().c_str(), ESPConnect.getCaptivePortalTimeout());
+      case Mycila::ESPConnect::State::PORTAL_STARTING:
+        logger.info(TAG, "Starting Captive Portal %s for %" PRIu32 " seconds...", espConnect.getAccessPointSSID().c_str(), espConnect.getCaptivePortalTimeout());
         break;
-      case ESPConnectState::PORTAL_STARTED:
-        logger.info(TAG, "Captive Portal started at %s with IP address %s", ESPConnect.getWiFiSSID().c_str(), ESPConnect.getIPAddress().toString().c_str());
+      case Mycila::ESPConnect::State::PORTAL_STARTED:
+        logger.info(TAG, "Captive Portal started at %s with IP address %s", espConnect.getWiFiSSID().c_str(), espConnect.getIPAddress().toString().c_str());
         break;
-      case ESPConnectState::PORTAL_COMPLETE: {
-        if (ESPConnect.hasConfiguredAPMode()) {
+      case Mycila::ESPConnect::State::PORTAL_COMPLETE: {
+        if (espConnect.hasConfiguredAPMode()) {
           logger.info(TAG, "Captive Portal: Access Point configured");
         } else {
           logger.info(TAG, "Captive Portal: WiFi configured");
         }
         break;
       }
-      case ESPConnectState::PORTAL_TIMEOUT:
+      case Mycila::ESPConnect::State::PORTAL_TIMEOUT:
         logger.warn(TAG, "Captive Portal: timed out.");
         break;
       default:
         break;
     }
   });
-  ESPConnect.begin(webServer, hostname, ssid, MYCILA_ADMIN_PASSWORD);
+  espConnect.begin(hostname, ssid, MYCILA_ADMIN_PASSWORD);
 
   // start tasks
   assert(coreTaskManager.asyncStart(1024 * 4, 1, 1, 100, true)); // NOLINT
