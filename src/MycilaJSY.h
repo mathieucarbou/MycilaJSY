94a3c9516510917b9432d15e490f5a39b09bdf1a
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-10-26 14:20:49 +0200

    v11.0.0

--- src/MycilaJSY.h
+++ src/MycilaJSY.h
@@ -12,9 +12,9 @@
   #include <ArduinoJson.h>
 #endif
 
-#define MYCILA_JSY_VERSION          "10.1.0"
-#define MYCILA_JSY_VERSION_MAJOR    10
-#define MYCILA_JSY_VERSION_MINOR    1
+#define MYCILA_JSY_VERSION          "11.0.0"
+#define MYCILA_JSY_VERSION_MAJOR    11
+#define MYCILA_JSY_VERSION_MINOR    0
 #define MYCILA_JSY_VERSION_REVISION 0
 
 // #define MYCILA_JSY_DEBUG 1
